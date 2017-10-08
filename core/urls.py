bbf8d563e13ceb4fc695fc50bbcc730daac7e91f
Francisco Manzano <fmanzano@zertifica.com>
2017-10-09 01:17:18 +0200

    static files serve

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
