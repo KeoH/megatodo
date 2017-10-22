bd41e1c142d96b1916c925c8fe348895ca96ba18
Francisco Manzano <fmanzano@zertifica.com>
2017-10-22 18:04:20 +0200

    Comments and task viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from .views import TodoViewSet, UserViewSet
+from .views import TodoViewSet, UserViewSet, TaskViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'todos', TodoViewSet, base_name='todo-list')
+router.register(r'tasks', TaskViewSet, base_name='task-list')
+router.register(r'comments', CommentViewSet, base_name='comment-list')
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
--- api/views.py
+++ api/views.py
@@ -1,22 +1,31 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from todos.models import Todo
+from todos.models import Todo, Task, Comment
 
 from .serializers.todo import TodoSerializer
+from .serializers.task import TaskSerializer
 from .serializers.user import UserSerializer
+from .serializers.comment import CommentSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
 
     def get_queryset(self):
-        print(type(self.request.auth))
         if self.request.user.is_authenticated:
             return Todo.objects.filter(author=self.request.user)
         else:
             return Todo.objects.none()
 
 
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
